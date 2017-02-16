5f655f4d3582b94c4589f3d193faa61fb6948492
David Tan <dt1041@nyu.edu>
2017-02-16 14:17:53 -0500

    Style the question show page

--- frontend/components/question/question_container.jsx
+++ frontend/components/question/question_container.jsx
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
 import Question from './question';
+import { fetchQuestion } from "../../actions/question_actions";
 
 const mapStateToProps = (state, ownProps) => {
+  const question = state.questions[ownProps.params.id];
+  const author = (question) ? question.author : null;
   return {
-  question: state.questions[ownProps.params.id]
+  question,
+  author
   };
 };
 
 const mapDispatchToProps = dispatch => ({
+  fetchQuestion: (id) => dispatch(fetchQuestion(id))
 });
 
 export default connect(
