e0aeec03c06eb25b5748780cac68a176464a6998
David Tan <dt1041@nyu.edu>
2017-02-16 16:40:27 -0500

    Can delete own questions

--- frontend/components/question/question.jsx
+++ frontend/components/question/question.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import Author from "./author";
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 
 export default class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteQuestion = this.deleteQuestion.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchQuestion(this.props.params.id);
   }
 
+  deleteQuestion(e) {
+    e.preventDefault();
+    this.props.deleteQuestion(this.props.params.id)
+    .then(hashHistory.push("/"));
+  }
+
   render() {
-    const {question, author} = this.props;
+    const {currentUser, question, author} = this.props;
+    let deleteButton = null;
+    if (currentUser && author && currentUser.id === author.id) {
+      deleteButton = (
+        <div>
+          <button onClick={this.deleteQuestion}> {"delete"} </button>
+        </div>
+      );
+    }
     return (
       <div className="question-show">
         <div className="header">
@@ -20,6 +38,7 @@ export default class Question extends Component {
           </Link>
         </div>
         <QuestionDetail question={question} author={author}/>
+        {deleteButton}
       </div>
     );
   }
