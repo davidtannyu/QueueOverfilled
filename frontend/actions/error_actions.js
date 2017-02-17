c44e091856a6687fffc714ea199b78194258a6e7
David Tan <dt1041@nyu.edu>
2017-02-17 16:49:31 -0500

    Clear errors on clicking root link

--- frontend/actions/error_actions.js
+++ frontend/actions/error_actions.js
@@ -1,7 +1,19 @@
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const DEFAULT_ERRORS = {
+  signUp: {errors: []},
+  logIn: {errors: []},
+  logOut: {errors: []},
+  question: {errors: []},
+  answer: {errors: []}
+};
 
 export const receiveErrors = (errors, formType) => ({
   type: RECEIVE_ERRORS,
   errors,
   formType
+});
+
+export const resetErrors = () => ({
+  type: RECEIVE_ERRORS,
+  errors: DEFAULT_ERRORS
 });
--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
-import { Link } from 'react-router';
+import RootLink from './root_link';
 
 const App = (props) => {
   const { children } = props;
@@ -8,11 +8,7 @@ const App = (props) => {
     <div >
       <div className="navbar">
         <div className="navbar-left">
-          <h1 className="logo">
-            <Link to="/">
-              Queue <strong>Overfilled</strong>
-            </Link>
-          </h1>
+          <RootLink />
         </div>
         <div className="navbar-right">
           <GreetingContainer />
