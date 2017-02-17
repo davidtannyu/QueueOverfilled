e6228e691c6eb9f5e890a4cf30e2f863db88a165
David Tan <dt1041@nyu.edu>
2017-02-17 16:57:04 -0500

    Undid root link clearing error. Handle it with last commit

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
-import RootLink from './root_link';
+import { Link } from 'react-router';
 
 const App = (props) => {
   const { children } = props;
@@ -8,7 +8,11 @@ const App = (props) => {
     <div >
       <div className="navbar">
         <div className="navbar-left">
-          <RootLink />
+          <h1 className="logo">
+            <Link to="/">
+              Queue <strong>Overfilled</strong>
+            </Link>
+          </h1>
         </div>
         <div className="navbar-right">
           <GreetingContainer />
