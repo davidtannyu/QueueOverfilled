1de1da595bc053f25c3910bd21363f98be5d57be
David Tan <dt1041@nyu.edu>
2017-02-21 16:42:58 -0500

    Basic text editor

--- frontend/components/text_editor.jsx
+++ frontend/components/text_editor.jsx
@@ -4,22 +4,39 @@ import {Link, hashHistory} from 'react-router';
 export default class TextEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = { preview: "" };
+    this.state = { preview: "", body: "" };
     this.handleField = this.handleField.bind(this);
     this.parseText = this.parseText.bind(this);
+    this.addStrongText = this.addStrongText.bind(this);
   }
 
   handleField(e) {
     e.preventDefault();
     this.props.handleField("body")(e);
-    this.setState( {preview: this.parseText(e.target.value)});
+    this.setState(
+      {
+        preview: this.parseText(e.target.value),
+        body: e.target.value
+    });
   }
 
   parseText(text) {
     let parsedText = text;
-    parsedText = parsedText.replace(/\*\*(.+)\*\*/,"<b>$1</b>");
-    parsedText = parsedText.replace(/\*(.+)\*/,"<i>$1</i>");
+    parsedText = parsedText.replace(/\*\*(.+)\*\*/g,"<b>$1</b>");
+    parsedText = parsedText.replace(/\*(.+)\*/g,"<i>$1</i>");
     return parsedText;
+  }
+
+  addStrongText(e) {
+    e.preventDefault();
+    let value;
+    if (!window.getSelection.toString()) {
+      value = this.state.body.replace(window.getSelection.to_s,"<b>$1</b>");
+    } else {
+      value = `${this.state.body}**strong**`;
+    }
+    e.target = { value };
+    this.handleField(e);
   }
 
   render() {
@@ -28,7 +45,7 @@ export default class TextEditor extends Component {
     return (
       <div className="text-editor">
         <div className="button-row">
-          <p onClick={console.log}>B</p>
+          <p onClick={this.addStrongText} value="**strong**">B</p>
           <p onClick={console.log}>I</p>
         </div>
         <textarea className="text-editor"
@@ -36,8 +53,7 @@ export default class TextEditor extends Component {
           value={value}>
         </textarea>
         <div className="preview">
-          <p className="text">
-            {this.state.preview}
+          <p className="text" dangerouslySetInnerHTML={{__html: this.state.preview}}>
           </p>
         </div>
       </div>
