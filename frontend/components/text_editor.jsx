7259b103bfbf65ebb524eaad9087dfb574f51b3a
David Tan <dt1041@nyu.edu>
2017-02-21 15:54:45 -0500

    Text editor will preview bold and italic

--- frontend/components/text_editor.jsx
+++ frontend/components/text_editor.jsx
@@ -4,12 +4,22 @@ import {Link, hashHistory} from 'react-router';
 export default class TextEditor extends Component {
   constructor(props) {
     super(props);
+    this.state = { preview: "" };
     this.handleField = this.handleField.bind(this);
+    this.parseText = this.parseText.bind(this);
   }
 
   handleField(e) {
     e.preventDefault();
     this.props.handleField("body")(e);
+    this.setState( {preview: this.parseText(e.target.value)});
+  }
+
+  parseText(text) {
+    let parsedText = text;
+    parsedText = parsedText.replace(/\*\*(.+)\*\*/,"<b>$1</b>");
+    parsedText = parsedText.replace(/\*(.+)\*/,"<i>$1</i>");
+    return parsedText;
   }
 
   render() {
@@ -26,7 +36,9 @@ export default class TextEditor extends Component {
           value={value}>
         </textarea>
         <div className="preview">
-
+          <p className="text">
+            {this.state.preview}
+          </p>
         </div>
       </div>
     );
