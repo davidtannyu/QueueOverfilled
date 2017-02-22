f2ca25d0114012baa640143e8b291ba5428e6b61
David Tan <dt1041@nyu.edu>
2017-02-22 17:52:51 -0500

    Define vote components

--- frontend/components/vote/vote.jsx
+++ frontend/components/vote/vote.jsx
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 
 export default class Vote extends Component {
-  render() {
-    return (
-      <div>
+  constructor(props) {
+    super(props);
+    const {vote_count, vote} = props;
+    this.state = { vote_count, vote };
+  }
 
+  render() {
+    const { vote } = this.state;
+    let upvote = "";
+    let downvote = "";
+    if (vote.value === 1) {
+      upvote = "selected";
+    } else if (vote.value === -1) {
+      downvote = "selected";
+    }
+    return (
+      <div className="vote-component">
+        <div className="upvote">
+          <p className={upvote}>
+            ^
+          </p>
+        </div>
+        <div className="vote_count">
+          { this.state.vote_count }
+        </div>
+        <div className="downvote">
+          <p className={downvote}>
+            V
+          </p>
+        </div>
       </div>
     );
   }
--- frontend/components/vote/vote_container.jsx
+++ frontend/components/vote/vote_container.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchVote } from "../../actions/vote_actions";
+import { createVote, updateVote } from "../../actions/vote_actions";
 import Vote from './vote';
 
 const mapStateToProps = (state, ownProps) => {
+  const {vote_count} = ownProps;
+  let currentVote = {};
+  Object.values(state.votes).each((vote) => {
+    if (vote.voter_id === state.currentUser.id &&
+      vote.answer_id === ownProps.answer_id) {
+        currentVote = vote;
+        return false;
+    }
+  });
   return {
+    currentVote,
+    vote_count
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
+    createVote: (vote) => dispatch(createVote(vote)),
+    updateVote: (vote) => dispatch(updateVote(vote))
   };
 };
 
