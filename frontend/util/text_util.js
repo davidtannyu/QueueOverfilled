75ea451c0ae3c76ee14127954d5258f62db66cbd
David Tan <dt1041@nyu.edu>
2017-02-23 14:51:45 -0500

    Sanitize preview text

--- frontend/util/text_util.js
+++ frontend/util/text_util.js
@@ -1,6 +1,10 @@
+const createDOMPurify = require('dompurify');
+const DOMPurify = createDOMPurify(window);
+
 export const parseText = (text) => {
   let parsedText = text;
   parsedText = parsedText.replace(/\*\*([^*][^*]+)\*\*/g,"<b>$1</b>");
   parsedText = parsedText.replace(/\*([^*]+)\*/g,"<i>$1</i>");
+  parsedText = DOMPurify.sanitize(parsedText);
   return parsedText;
 };
