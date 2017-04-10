48da7010c27a95667ea3233cd0176bf2ac0d0a3e
David Tan <dt1041@nyu.edu>
2017-04-10 15:47:31 +0000

    Return empty object for selector

--- frontend/reducers/selector.js
+++ frontend/reducers/selector.js
@@ -1,4 +1,7 @@
-export const getCurrentVote = ({votes, currentUser}, answerId) => {
+export const getCurrentVote = ({votes, currentUser }, answerId) => {
+    if (!currentUser) {
+        return {};
+    }
     Object.values(votes).forEach((vote) => {
         if (currentUser && vote.voter_id === currentUser.id &&
             vote.answer_id === answerId) {
