dd5daa4fd9b23a8e50a11223a5b1f52bd4d28d6a
David Tan <dt1041@nyu.edu>
2017-06-17 01:19:29 +0000

    Refactor to one line

--- frontend/components/session_form/session_form_container.jsx
+++ frontend/components/session_form/session_form_container.jsx
@@ -4,8 +4,7 @@ import { login, signup } from '../../actions/user_actions';
 import { receiveErrors } from '../../actions/error_actions';
 
 const mapStateToProps = (state, ownProps) => {
-  let loggedIn = state.currentUser !== null;
-  let formType = ownProps.route.formType;
+  let loggedIn = state.currentUser !== null, formType = ownProps.route.formType;
   return ({
   errors: state.forms[formType].errors,
   formType,
